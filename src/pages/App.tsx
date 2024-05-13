3b5b229253ecd1fce85df7de3d7d371eb1adcec0
songsong <dragonh0919@gmail.com>
2024-05-14 01:11:30 +0900

    move repository

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -6,9 +6,7 @@ export default function Home(): ReactElement {
   return (
     <div className={st.container}>
       <p>이곳은 메인 페이지 입니다</p>
-      <Link to="/qrtag">QR 태그 화면으로 넘어가기</Link>
       <Link to="/chat">Chat 화면으로 넘어가기</Link>
-      <Link to="/modal">모달 화면</Link>
     </div>
   );
 }
